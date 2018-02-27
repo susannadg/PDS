1105861ac1a3b6430385179d7f7f673141940ddb
susannadg <36883017+susannadg@users.noreply.github.com>
2018-02-27 16:53:06 +0100

    Løst opaver 1 og 2
    1 og 2

--- opgaver/04_opgaver.R
+++ opgaver/04_opgaver.R
@@ -22,8 +22,20 @@ seats <- read_csv("https://raw.githubusercontent.com/jespersvejgaard/PDS/master/
 # Bemærk: Bliver ae, ao og aa skrevet forkert? Så klik filer -> reopen with encoding -> vælg UTF-8 -> sæt flueben i "Set as default..." -> OK
 
 # 1. Eksekvér præamblen ovenfor 
-# 2. Skriv en funktion, som tager to vektorer som argumenter, summerer dem og returnerer resultatet. 
-# 3. Skriv en funktion, som tager en vektor som argument, og som beregner standardafvigelsen hvis n > 30, og ellers printer "N er under 30!" og afbryder (hint: `break`). Test med vektorerne `x`, `y`, `z` ovenfor.  
+  #done
+# 2. Skriv en funktion, som tager to vektorer som argumenter, summerer dem og returnerer resultatet.
+function_sum <- function(x,y) {
+  print(sum(x,y))
+}
+function_sum(x,y)
+
+# #3. Skriv en funktion, som tager en vektor som argument, og som beregner 
+#   standardafvigelsen hvis n > 30, og ellers printer "N er under 30!" og afbryder (hint: `break`).
+#   Test med vektorerne `x`, `y`, `z` ovenfor.  
+function(x) {
+  if x>30 {sd(x)}
+}
+
 # 4. Skriv et for-loop, som looper igennem alle kolonnerne i dataframen `seats` og beregner partiernes gennemsnitlige antal mandater
 # 5. Brug `lapply()`, `sapply()`, `vapply()` eller `map_dbl()` til at beregne det samme
 # 6. Skriv en funktion, som simulerer et terningekast (hint: `sample()`)
